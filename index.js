b575f9b0291418fd8e8b86d74f0ebf19e1b35bea
ziniulian <ziniulian@163.com>
2019-01-08 10:39:18 +0800

    简单对接

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var clsBuf = global.Buffer || require("buffer").Buffer;
 
 var tools = {
 	ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-	port: process.env.OPENSHIFT_NODEJS_PORT || 80,
+	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	socket: null,
 	key: "GET /" + (process.env.OPENSHIFT_NODEJS_NATKEY || "zi%niu%lian") + "/ HTTP",
 	ss: {},
@@ -16,8 +16,8 @@ var tools = {
 		s.on("data", function(dat) {
 			bs = clsBuf.concat([bs, dat]);
 			if (bs.indexOf("\r\n\r\n") > 0) {
-// console.log(bs.toString());
-				if (dat.slice(0, tools.key.length).toString("utf8") === tools.key) {
+// console.log(bs.toString("utf8"));
+				if (dat.toString("utf8", 0, tools.key.length) === tools.key) {
 					s.removeAllListeners("data");
 					s.on("error", function (e) {
 						tools.endSocket();
@@ -25,7 +25,7 @@ var tools = {
 					});
 					s.on("end", tools.endSocket);
 					tools.initSocket(s);
-					s.write("HTTP/1.1 200\r\nConnection: keep-alive\r\n\r\n");
+					console.log("OK!");
 					tools.socket = s;
 				} else {
 					tools.err(s);
@@ -60,17 +60,18 @@ var tools = {
 
 	endSocket: function () {
 		var i, s;
-		for (var i = 0; i < tools.max) {
+		for (i = 0; i < tools.max; i ++) {
 			s = tools.ss[i];
 			if (s) {
 				s.end();
 			}
 		}
 		tools.socket = null;
+		console.log("END!");
 	},
 
 	endSubSocket: function (id) {
-		tools.socket.write(id + ",end,");
+		tools.socket.write("HTTP/1.1 200 -" + id + "\r\nConnection: keep-alive\r\nContent-Length: 1\r\n\r\n1");
 		delete tools.ss[id];
 	},
 
@@ -85,15 +86,16 @@ var tools = {
 			s.end();
         });
         s.on("end", function () {
+			console.log("Srv end : " + s.id);
 			tools.endSubSocket(s.id);
         });
 		s.on("data", function (dat) {
-			console.log("<<! --- s");
-			console.log("<<",dat.length);
-			console.log("<<",dat.toString());
-			console.log("<<! --- e");
+			// console.log("Srv <<! --- s");
+			console.log("Srv << ", s.id + " , " + dat.length);
+			// console.log(dat.toString("utf8"));
+			// console.log("Srv <<! --- e");
 			tools.socket.write(clsBuf.concat([
-				new clsBuf(s.id + "," + data.length + ","),
+				new clsBuf("HTTP/1.1 200 " + s.id + "\r\nConnection: keep-alive\r\nContent-Length: " + dat.length + "\r\n\r\n", "utf8"),
 				dat
 			]));
         });
@@ -104,12 +106,10 @@ var tools = {
 		var cs = null;	// 未接收完的连接
 		var n = 0;		// 未接收完的字节数
 		ss.on("data", function (dat) {
-			console.log(">>! --- s");
-			console.log(">>",dat.length);
-			console.log(">>", dat.toString());
-			console.log(">>! --- e");
-			var b = true;
+			var id, b = true;
 			if (n) {
+				// 对未能一次性收齐的数据进行处理
+				id = cs.id;
 				if (dat.length < n) {
 					cs.write (dat);
 					n -= dat.length;
@@ -119,23 +119,62 @@ var tools = {
 					n = 0;
 					cs = null;
 					b = false;
+					bs = new clsBuf(0);
 				} else {
+					// 一个新的请求接在前一个数据的尾部，是不太可能发生的情况
 					cs.write (dat.slice(0, n));
+					bs = dat.slice(n);
 					n = 0;
 					cs = null;
-					bs = clsBuf.concat([bs, dat.slice(n)]);
 				}
 			} else {
 				bs = clsBuf.concat([bs, dat]);
 			}
 
 			if (b) {
-				if (bs.indexOf(",") > 0) {
+				b = bs.indexOf("\r\n\r\n");
+				if (b > 0) {
+					b += 4;
+					var t = bs.toString("utf8", 0, b);
+					id = t.substring(8, t.indexOf(" HTTP", 8)) - 0;
+					switch (t.substring(0, 8)) {
+						case "POST /R/":
+							var d = bs.slice(b);
+							var nn = t.indexOf("Content-Length: ", 20) + 16;
+							nn = t.substring(nn, t.indexOf("\r\n", nn)) - 0;
+
+							// console.log("Srv >>! --- s");
+							console.log("Srv >> ", id + " , " + nn);
+							// console.log(d.toString("utf8"));
+							// console.log("Srv >>! --- e");
+
+							if (d.length < nn) {
+								// 数据未能一次性收齐
+								cs = tools.ss[id];
+								n = nn;
+								cs.write (d);
+							} else {
+								tools.ss[id].write (d);
+								if (d.length > nn) {
+									// 一个新的请求接在前一个数据的尾部，是不太可能发生的情况。直接报错
+									console.log("--- Err --- " + d.length + " , " + nn));
+								}
+							}
+							break;
+						case "GET /End":
+							var s = tools.ss[id];
+							if (s) {
+								s.end();
+							}
+							break;
+					}
+					bs = new clsBuf(0);
+				}
 			}
 		});
 	}
 
-}
+};
 
 function mainSrv (s) {
 	if (tools.socket) {
