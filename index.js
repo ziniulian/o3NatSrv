320abaf81104cba2a72fe681fba82116f2eb64f1
ziniulian <ziniulian@163.com>
2019-01-12 10:54:25 +0800

    双工对接测试

--- index.js
+++ index.js
@@ -1,43 +1,120 @@
-// LZR 模块加载
-require("lzr");
-
-// LZR 子模块加载
-LZR.load(["LZR.Node.Srv"]);
+var net = require("net");
 
 // 内网渗透对象
 var nato = {
-	socket: null,
-	keepLink: 0,
 	clsBuf: global.Buffer || require("buffer").Buffer,
 	pwd: process.env.OPENSHIFT_NODEJS_NATPWD || "pwd",
+	pwdLen: 3,
+	port: 8080,
+	rs: null,	// 接收端
+	ws: null,	// 发送端
+	keepLink: 0,
 	ktim: 20000,
-	max: 3,
-	rs: {},
+	max: 100,
+	os: {},		// 其它连接
 	sn: 0,
+
+	// 监听器
+	listener: function (s) {
+		if (nato.keepLink) {
+			if (nato.rs) {
+				s.on("error", nato.hdErr);
+				s.on("end", nato.hdEnd);
+nato.ws.write(s.remoteAddress + ":" + s.remotePort + ",");
+				s.write("HTTP/1.1 200\r\nConnection: close\r\nContent-Length: 3\r\n\r\nOK!");
+				s.end();
+			} else {
+				s.on("error", nato.hdErr);
+				s.on("end", nato.hdEnd);
+				s.on("data", nato.chkRs);
+			}
+		} else {
+			s.on("error", nato.hdErr);
+			s.on("end", nato.hdEnd);
+			s.on("data", nato.chkWs);
+		}
+	},
 
 	// 密码校验
 	chkPwd: function (p) {
 		return p === nato.pwd;
 	},
 
-	// 对接
-	lnk: function (s) {
-		nato.socket = s;
-		s.removeAllListeners("data");
-		s.removeAllListeners("error");
-		s.removeAllListeners("end");
-		s.on("error", nato.endLnk);
-		s.on("end", nato.endLnk);
-		s.write("HTTP/1.1 200\r\nConnection: keep-alive\r\n\r\nO,");
-		nato.keepLink = setInterval(nato.kpLnk, nato.ktim);
+	// 检测发送端
+	chkWs: function (dat) {
+		var n = 9 + nato.pwdLen;
+		if ((dat.length > n) && (dat.toString("utf8", 5, 9) === "/ws/") && (dat.toString("utf8", n, (n + 2)) === "/ ") && nato.chkPwd(dat.toString("utf8", 9, n))) {
+			nato.ws = this;
+			nato.keepLink = setInterval(nato.kpLnk, nato.ktim);
+			this.removeAllListeners("data");
+			this.removeAllListeners("error");
+			this.removeAllListeners("end");
+			this.on("error", nato.wsErr);
+			this.on("end", nato.wsEnd);
+			// this.on("data", nato.hdRs);
+			this.write("HTTP/1.1 200\r\nConnection: keep-alive\r\n\r\nW,");
+		} else {
+			nato.err(this, "no WS!");
+		}
 	},
 
 	// 保持对接
 	kpLnk: function () {
-		if (!nato.wrt("T,")) {
-			nato.endLnk();
+		if (nato.ws) {
+			nato.ws.write("L,");
 		}
 	},
+
+	// 发送端错误处理
+	wsErr: function (e) {
+console.log ((this === nato.ws?"WS":"RS") + "_Err : " + e.message);
+		nato.wsEnd();
+	},
+
+	// 终止发送端
+	wsEnd: function () {
+		if (nato.keepLink) {
+			clearInterval(nato.keepLink);
+			nato.keepLink = 0;
+			// 清空所有其它连接 ...
+			if (nato.ws) {
+				nato.ws.write("E,");
+			}
+		}
+		if (nato.rs) {
+			nato.rs.end();
+			nato.rs = null;
+		}
+		if (nato.ws) {
+			nato.ws.end();
+			nato.ws = null;
+		}
+	},
+
+	// 检测接收端
+	chkRs: function (dat) {
+		var n = 9 + nato.pwdLen;
+		if ((dat.length > n) && (dat.toString("utf8", 5, 9) === "/rs/") && (dat.toString("utf8", n, (n + 2)) === "/ ") && nato.chkPwd(dat.toString("utf8", 9, n))) {
+			nato.rs = this;
+			this.removeAllListeners("data");
+			this.removeAllListeners("error");
+			this.removeAllListeners("end");
+			this.on("error", nato.wsErr);
+			this.on("end", nato.wsEnd);
+			this.on("data", nato.hdRs);
+			nato.ws.write("R,");
+		} else {
+			nato.err(this, "no RS!");
+		}
+	},
+
+	// 接收信息
+	hdRs: function (dat) {
+console.log(dat.toString("utf8"));
+	},
+
+
+/*************************************************/
 
 	// 停止对接
 	endLnk: function () {
@@ -136,142 +213,25 @@ var nato = {
 		}
 	},
 
+
+/**********************************************/
+
 	// 错误回复
-	err: function (r) {
-		if (r.connection) {
-			// 已经应答过的 res 没有 connection 属性，若再次应答将会报错！
-			try {
-				if (nato.keepLink) {
-					// r.status(408).send("超时！");
-					r.set({"Retry-After": "300"});	// 过载延迟时间
-					r.status(503).send("过载！");
-				} else {
-					r.status(404).send("Hi!");
-				}
-			} catch (e) {
-				// console.log(e.message);
-			}
-		}
+	err: function (s, msg) {
+		s.write("HTTP/1.1 404\r\nConnection: close\r\nContent-Length: " + msg.length + "\r\n\r\n" + msg);
+		s.end();
+	},
+
+	// 错误处理
+	hdErr: function (e) {
+console.log(e);
+		this.end();
+	},
+	hdEnd: function () {
+console.log("end");
 	}
 };
 
-// 服务的实例化
-var srv = new LZR.Node.Srv ({
-	ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-	port: process.env.OPENSHIFT_NODEJS_PORT || 8080
-});
-nato.ktim -= 0;
 
-// 内网渗透_对接
-srv.ro.post("/LZR/nat/lnk/:pwd/", function (req, res) {
-	if (!nato.keepLink && nato.chkPwd(req.params.pwd)) {
-		nato.lnk(req.socket);
-	} else {
-		nato.err(res);
-	}
-});
-
-// 内网渗透_停止对接
-srv.ro.post("/LZR/nat/endLnk/:pwd/", function (req, res) {
-	if (nato.keepLink && req.params.pwd === nato.pwd) {
-		nato.endLnk();
-		res.send("OK");
-	} else {
-		nato.err(res);
-	}
-});
-
-// 内网渗透_获取请求
-srv.ro.post("/LZR/nat/getReq/:id/", function (req, res) {
-	var o = nato.rs[req.params.id];
-	if (o && nato.keepLink) {
-		res.json(o.c);
-	} else {
-		res.json({ok:false});
-	}
-});
-
-// 内网渗透_发送应答
-srv.ro.post("/LZR/nat/sendRes/:id/", function (req, res) {
-	var o = nato.rs[req.params.id];
-	if (o && nato.keepLink) {
-		var d = [];
-		var size = 0;
-		req.on("data", function (dat) {
-			d.push(dat);
-			size += dat.length;
-		});
-		req.on("end", function () {
-			o.res.send(nato.clsBuf.concat(d, size).toString("utf8"));
-			nato.del(o.id);
-		});
-	}
-	res.send("OK");
-});
-
-// 内网渗透_断开连接
-srv.ro.post("/LZR/nat/end/:id/", function (req, res) {
-	if (nato.keepLink) {
-		nato.del(req.params.id);
-	}
-	res.send("OK");
-});
-
-/******** 代理服务 ********/
-// 代理_创建连接
-srv.ro.post("/LZR/prx/end/:id/:host/:port/", function (req, res) {
-	res.send("创建连接");
-});
-
-// 代理_发送请求
-srv.ro.post("/LZR/prx/sendReq/:id/", function (req, res) {
-	res.send("发送请求");
-});
-
-// 代理_获取应答
-srv.ro.post("/LZR/prx/getRes/:id/", function (req, res) {
-	res.send("获取应答");
-});
-
-// 代理_断开连接
-srv.ro.post("/LZR/prx/end/:id/", function (req, res) {
-	res.send("断开连接");
-});
-
-/******** 内网渗透 ********/
-srv.ro.get("*", function (req, res) {
-	var o = nato.crt(req, res);
-	if (o) {
-		nato.emit(o);
-	} else {
-		nato.err(res);
-	}
-});
-
-srv.ro.post("*", function (req, res) {
-	var o = nato.crt(req, res);
-	if (o) {
-		var d = [];
-		var size = 0;
-		req.on("data", function (dat) {
-			d.push(dat);
-			size += dat.length;
-		});
-		req.on("end", function () {
-			if (size) {
-				o.c.b = nato.clsBuf.concat(d, size);		// 主体 body
-			}
-			nato.emit(o);
-		});
-	} else {
-		nato.err(res);
-	}
-});
-
-// 错误处理
-srv.use("*", function (req, res) {
-	nato.err(res);
-});
-
-srv.start();
-console.log("LZRnatsrv start " + srv.ip + ":" + srv.port);
+net.createServer(nato.listener).listen(nato.port);
+console.log("LZRnatsrv start in " + nato.port);
