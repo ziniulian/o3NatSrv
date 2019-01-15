2ec7398fc429608dd7329aa1210606769a5377fb
ziniulian <ziniulian@163.com>
2019-01-15 10:11:22 +0800

    单通道可重连对接测试结果

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ var nato = {
 	ktim: 15000,
 	max: 100,
 
+	// 重连测试数据
+	reLinkTestDat: [],
+
 	// 监听器
 	listener: function (s) {
 		s.on("error", nato.hdErr);
 		if (nato.socket) {
-			nato.test(s, "OK!");
+			nato.send(s, "OK", nato.clsBuf.from(JSON.stringify(nato.reLinkTestDat)));
 		} else {
 			s.on("data", nato.lnk);
 		}
@@ -37,15 +40,12 @@ var nato = {
 	lnk: function (dat) {
 		var n = 10 + nato.pwdLen;
 		if ((dat.length > n) && (dat.toString("utf8", 5, 10) === "/lnk/") && (dat.toString("utf8", n, (n + 2)) === "/ ") && nato.chkPwd(dat.toString("utf8", 10, n))) {
-			nato.socket = this;
 			this.removeAllListeners("data");
-			this.removeAllListeners("error");
-			this.on("error", nato.endLnk);
-			this.on("end", nato.endLnk);
 			this.on("data", nato.hdDat);
-			nato.initLnk();
+			nato.size = 0;
+			nato.send(this, "lnk");
 		} else {
-			nato.test(this, "no link");
+			nato.test(this, "<H1>no! No! NO!</H1>");
 		}
 	},
 
@@ -56,66 +56,81 @@ var nato = {
 			nato.keepLink = 0;
 		}
 		if (nato.socket) {
-			var s = nato.socket;
 			nato.socket = null;
-			// 清空所有任务
-			// 清空连接池
-			s.end();
+			nato.reLinkTestDat.push(Date.now() + " : cut");
+			// console.log("已断开 : " + Date.now());
 		}
-console.log("link end!");
-	},
-
-	// 对接初始化
-	initLnk: function () {
-		nato.size = 0;
-		nato.sn = 0;
-		nato.ws = [];
-		nato.rs = {};
-		nato.kepLnk();
-console.log("linked");
-	},
-
-	// 保持对接，发送心跳
-	kepLnk: function () {
-		nato.keepLink = 0;
-		nato.socket.write("HTTP/1.1 200 lnk\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n");
+		this.end();
 	},
 
 	// 接收信息
 	hdDat: function (dat) {
-		if (nato.size) {
+		if (nato.size === 0) {
+			var k = dat.indexOf("\r\n\r\n");
+			if (k > 0) {
+				switch (dat.toString("utf8", 6, 9)) {
+					case "lnk":	// 心跳
+						if (!nato.socket) {
+							nato.socket = this;
+							this.removeAllListeners("error");
+							this.on("error", nato.endLnk);
+							this.on("end", nato.endLnk);
+							nato.reLinkTestDat.push(Date.now() + " : link");
+							// console.log("已连接 : " + Date.now());
+						}
+						nato.hdRtn(true);
+						break;
+					case "rtn":	// 反馈
+						nato.hdRtn();
+						break;
+					case "wrk":	// 任务
+						var i = dat.indexOf("Content-Length: ") + 16;
+						var j = dat.indexOf("\r\n", i, "utf8");
+						nato.size = dat.toString("utf8", i, j);
+						i = k + 4;	// 数据起始位置
+						j = dat.length - i;		// 数据实际长度
+						if (nato.size > j) {
+							nato.size -= j;
+							nato.buf = [dat.slice(i)];
+						} else {
+							k = i + nato.size;
+							nato.size = 0;
+							nato.doWrk(dat.slice(i, k));
+							if (nato.size < j) {
+								console.log("理论上不会出现的数据溢出");
+								// nato.hdDat(dat.slice(i + nato.size));
+							}
+						}
+						break;
+				}
+			} else if (dat.indexOf("\n\n")) {
+				console.log("不规范的结束符");
+				natc.endLnk.call(this);
+			} else {
+				console.log("理论上不太会出现的HTTP头信息不完整");
+				nato.buf = dat;
+				nato.size = -1;
+			}
+		} else if (nato.size > 0) {
+			var d = false;
+			if (dat.length > nato.size) {
+				// 理论上不会出现的数据溢出
+				d = dat.slice(nato.size);
+				dat = dat.slice(0, nato.size);
+			}
 			nato.buf.push(dat);
 			nato.size -= dat.length;
 			if (nato.size === 0) {
 				nato.doWrk(nato.clsBuf.concat(nato.buf));
-			} else if (nato.size < 0) {
-				// 有问题，不应出现这种状况
-console.log("数据溢出");
-				nato.endLnk();
+				if (d) {
+					console.log("理论上不会出现的数据溢出");
+					// nato.hdDat(d);
+				}
 			}
 		} else {
-			switch (dat.toString("utf8", 6, 9)) {
-				case "lnk":	// 心跳
-					nato.hdRtn(true);
-					break;
-				case "rtn":	// 反馈
-					nato.hdRtn();
-					break;
-				case "wrk":	// 任务
-					var i = dat.indexOf("Content-Length: ") + 16;
-					var j = dat.indexOf("\r\n", i, "utf8");
-					nato.size = dat.toString("utf8", i, j);
-					i = dat.indexOf("\r\n\r\n", j, "utf8") + 4;
-					j = dat.length - i;
-					if (nato.size === j) {
-						nato.size = 0;
-						nato.doWrk(dat.slice(i));
-					} else {
-						nato.size -= j;
-						nato.buf = [dat.slice(i)];
-					}
-					break;
-			}
+			console.log("补充理论上不太会出错的HTTP头信息");
+			nato.size = 0;
+			nato.hdDat(nato.clsBuf.concat([nato.buf, dat]));
 		}
 	},
 
@@ -128,6 +143,12 @@ console.log("数据溢出");
 		} else {
 			nato.kepLnk();
 		}
+	},
+
+	// 保持对接，发送心跳
+	kepLnk: function () {
+		nato.keepLink = 0;
+		nato.send(nato.socket, "lnk");
 	},
 
 	// 发送任务
@@ -157,9 +178,23 @@ console.log("数据溢出");
 		}
 	},
 
+	// 发送信息
+	send: function (s, nam, dat) {
+		var d = "HTTP/1.1 200 " + nam + "\r\nConnection: keep-alive\r\nContent-Length: ";
+		if (dat) {
+			d = nato.clsBuf.concat ([
+				nato.clsBuf.from (d + dat.length + "\r\n\r\n"),
+				dat
+			]);
+		} else {
+			d += "0\r\n\r\n";
+		}
+		s.write(d);
+	},
+
 	// 测试回复
 	test: function (s, msg) {
-		s.write("HTTP/1.0 503\r\nConnection: close\r\nContent-Length: " + msg.length + "\r\n\r\n" + msg);
+		s.write("HTTP/1.1 503\r\nConnection: close\r\nContent-Length: " + msg.length + "\r\n\r\n" + msg);
 		s.end();
 	},
 
