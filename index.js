4e4b595578e10e44462954e60f1c8d25647eac80
ziniulian <ziniulian@163.com>
2019-01-17 13:30:45 +0800

    改回单发模式

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ var nato = {
 	port: 8080,
 
 	socket: null,
-	size: 0,		// 尚未接收完的数据大小
-	buf: null,	// 已接收到的数据缓存
-	keepLink: 0,	// 心跳ID
-	ws: [],		// 任务堆
-	pw: null,	// 回滚任务堆
+	buf: null,	// 头部不足的缓存
+	twrk: null,	// 未接收完数据的任务
+	swrk: 0,	// 一次任务请求的总数据长度
+
+	// ws: [],		// 任务堆
+	// pw: null,	// 回滚任务堆
 	rs: {},		// 连接池
 	sn: 0,
 
-	ktim: 5000,
+	keepLink: 0,
+	ktim: 17000,
 	max: 100,
 
 	// 监听器
 	listener: function (s) {
+		// srv.getConnections (function (e, n) {
+		// 	console.log("异步获取当前连接数 ： " + n);
+		// });
+		// console.log(srv.listenerCount() + " / " + srv.getMaxListeners() + " , " + srv._connections + " / " + srv.maxConnections);
 		s.on("error", nato.hdErr);
-		// s.on("data", nato.first);
 		s.on("data", nato.lnk);
 	},
 
@@ -33,34 +38,57 @@ var nato = {
 
 	// 对接
 	lnk: function (dat) {
-		if ((dat.length > nato.pwdLen) && (dat.toString("utf8", 5, 10) === "/lnk/") && (dat.toString("utf8", nato.pwdLen, (nato.pwdLen + 2)) === "/ ") && nato.chkPwd(dat.toString("utf8", 10, nato.pwdLen))) {
+		var e = false;
+		if ((dat.length > nato.pwdLen) && (dat.toString("utf8", 5, 10) === "/lnk/") && (dat.toString("utf8", nato.pwdLen, (nato.pwdLen + 10)) === "/ HTTP/1.1") && nato.chkPwd(dat.toString("utf8", 10, nato.pwdLen))) {
 			if (nato.socket) {
-				// (this.remoteAddress === nato.socket.remoteAddress) // O3内网的IP每次连接都可能不一样，故此判断没意义。
 				nato.endLnk.call(nato.socket);
 			}
 			nato.socket = this;
-			nato.size = 0;
 			this.removeAllListeners("data");
 			this.removeAllListeners("error");
 			this.on("error", nato.endLnk);
 			this.on("end", nato.endLnk);
-			this.on("data", nato.hdDat);
-			nato.send(this, "lnk");
+			this.write("HTTP/1.1 200 lnk\r\nConnection: keep-alive\r\n\r\n");
+			nato.kepLnk();
 			console.log("已连接 : " + Date.now());
 		} else if (nato.socket) {
-			nato.addWrk(this, dat);
+			if (dat.indexOf("POST /wrk/ ") === 0) {
+				if ((nato.swrk === 0) && (dat.indexOf("\r\n\r\n") > 0)) {
+					this.removeAllListeners("data");
+					this.on("data", nato.hdWrk);
+					nato.hdWrk.call(this, dat);
+				} else {
+					this.end();
+				}
+			} else if (srv._connections < 7) {
+				var id = this.id || nato.getId(this);
+				if (id) {
+					this.removeAllListeners("data");
+					this.removeAllListeners("error");
+					this.on("error", nato.endDat);
+					this.on("end", nato.endDat);
+					this.on("data", nato.hdDat);
+					nato.hdDat.call(this, dat);
+				} else {
+					e = true;
+				}
+			} else {
+				e = true;
+			}
 		} else {
-			this.write("HTTP/1.1 503 nle\r\nConnection: close\r\nContent-Length: 20\r\n\r\n<H1>no! No! NO!</H1>");
-			// this.write("HTTP/1.1 503 LZR\r\nConnection: close\r\nContent-Length: 135\r\n\r\n<style type=\"text/css\">body,iframe{padding:0;margin:0;width:100%;height:100%;}</style><iframe src=\"https://www.ziniulian.tk/\"></iframe>");
+			e = true;
+		}
+		if (e) {
+			this.write("HTTP/1.1 302\r\nLocation: https://www.ziniulian.tk/\r\nConnection: close\r\nContent-Length: 0\r\n\r\n");
+			this.end();
 		}
 	},
 
 	// 停止对接
 	endLnk: function () {
-		this.removeAllListeners("data");
 		this.removeAllListeners("error");
 		this.removeAllListeners("end");
-		this.on("error", natc.hdErr);
+		this.on("error", nato.hdErr);
 		this.end();
 		if (nato.keepLink) {
 			clearTimeout(nato.keepLink);
@@ -70,147 +98,194 @@ var nato = {
 			nato.socket = null;
 			console.log("已断开 : " + Date.now());
 		}
-	},
-
-	// 交互信息处理
-	hdDat: function (dat) {
-		if (nato.size === 0) {
-			var k = dat.indexOf("\r\n\r\n");
-			if (k > 0) {
-				switch (dat.toString("utf8", 6, 9)) {
-					case "lnk":	// 心跳
-						nato.hdRtn(true);
-						break;
-					case "rtn":	// 反馈
-						nato.hdRtn();
-						break;
-					case "wrk":	// 任务
-						var i = dat.indexOf("Content-Length: ") + 16;
-						var j = dat.indexOf("\r\n", i, "utf8");
-						nato.size = dat.toString("utf8", i, j);
-						i = k + 4;	// 数据起始位置
-						j = dat.length - i;		// 数据实际长度
-						if (nato.size > j) {
-							nato.size -= j;
-							nato.buf = [dat.slice(i)];
-						} else {
-							k = i + nato.size;
-							nato.size = 0;
-							nato.doWrk(dat.slice(i, k));
-							if (nato.size < j) {
-								console.log("理论上不会出现的数据溢出");
-								// nato.hdDat(dat.slice(i + nato.size));
-							}
-						}
-						break;
-				}
-			} else if (dat.indexOf("\n\n")) {
-				console.log("不规范的结束符");
-				nato.endLnk.call(this);
-			} else {
-				console.log("理论上不太会出现的HTTP头信息不完整");
-				nato.buf = dat;
-				nato.size = -1;
-			}
-		} else if (nato.size > 0) {
-			var d = false;
-			if (dat.length > nato.size) {
-				// 理论上不会出现的数据溢出
-				d = dat.slice(nato.size);
-				dat = dat.slice(0, nato.size);
-			}
-			nato.buf.push(dat);
-			nato.size -= dat.length;
-			if (nato.size === 0) {
-				nato.doWrk(nato.clsBuf.concat(nato.buf));
-				if (d) {
-					console.log("理论上不会出现的数据溢出");
-					// nato.hdDat(d);
-				}
-			}
-		} else {
-			console.log("补充理论上不太会出错的HTTP头信息");
-			nato.size = 0;
-			nato.hdDat(nato.clsBuf.concat([nato.buf, dat]));
-		}
-	},
-
-	// 反馈处理
-	hdRtn: function (isLnk) {
-		if (nato.ws.length) {
-			nato.sendWrk();
-		} else if (isLnk) {
-			nato.keepLink = setTimeout(nato.kepLnk, nato.ktim);
-		} else {
-			nato.kepLnk();
-		}
+		// TODO: 任务回滚
 	},
 
 	// 保持对接，发送心跳
 	kepLnk: function () {
-		nato.keepLink = 0;
-		nato.send(nato.socket, "lnk");
+		if (nato.socket) {
+			if (nato.keepLink) {
+				nato.socket.write("0.");
+			}
+			nato.keepLink = setTimeout(nato.kepLnk, nato.ktim);
+		} else {
+			nato.keepLink = 0;
+		}
 	},
 
-	// 发送任务
-	sendWrk: function () {
-		// ... 主内容 ...
-
-		nato.pw = nato.ws;
-		nato.ws = [];
+	// 任务处理
+	hdWrk: function (dat) {
+console.log("<< -- " + dat.length);
+		var i, j, p, id;
+		p = 0;	// 指针
+		if (nato.buf) {
+			nato.buf.push(dat);
+			dat = nato.clsBuf.concat(nato.buf);
+			nato.buf = null;
+		} else if (nato.swrk === 0) {
+			p = dat.indexOf("\r\n\r\n") + 4;
+			i = dat.indexOf("Content-Length: ", 18, "utf8") + 16;
+			j = dat.indexOf("\r\n", i, "utf8");
+			nato.swrk = dat.toString("utf8", i, j) - 0;
+			// dat = dat.slice(p);
+			if (nato.twrk) {
+				// 数据传输出现了错误！理论不太可能发生。
+				console.log("hdWrk err twrk");
+				nato.twrk = null;
+			}
+		}
+		if (nato.swrk) {
+			nato.swrk += p;
+			while (dat.length > p) {
+				if (nato.twrk) {
+					i = dat.length - p;
+					if (i < nato.twrk.size) {
+						nato.twrk.size -= i;
+						nato.twrk.buf.push(dat.slice(p));
+						p = dat.length;
+					} else {
+						j = p + nato.twrk.size;
+						nato.twrk.buf.push(dat.slice(p, j));
+						nato.doWrk(nato.twrk.id, nato.clsBuf.concat(nato.twrk.buf));
+						nato.twrk = null;
+						p = j;
+					}
+				} else {
+					i = dat.indexOf(".", p);
+					if (i > 0) {
+						id = dat.toString("utf8", p, i) - 0;
+						i ++;
+						if (id === 0) {
+							p = i;
+						} else {
+							j = dat.indexOf(".", i);
+							if (j > 0) {
+								p = dat.toString("utf8", i, j) - 0;
+								if (p) {
+									nato.twrk = {
+										id: id,
+										buf: [],
+										size: p,
+									};
+								} else {
+									nato.clrSub(id);
+								}
+								p = j + 1;
+							} else {
+								nato.buf = [dat.slice(p)];
+								p = dat.length;
+							}
+						}
+					} else {
+						nato.buf = [dat.slice(p)];
+						p = dat.length;
+					}
+				}
+			}
+			nato.swrk -= dat.length;
+		}
+		if (nato.swrk <= 0) {
+			if (nato.swrk || nato.twrk || nato.buf) {
+				// 数据传输出现了错误！理论不太可能发生。
+console.log("hdWrk err ...");
+console.log(nato.swrk);
+console.log(nato.twrk);
+console.log(nato.buf);
+console.log("hdWrk err_end!");
+				this.write("HTTP/1.1 404 wER\r\nConnection: close\r\nContent-Length: 0\r\n\r\n");
+				this.end();
+			} else {
+				this.write("HTTP/1.1 200 wOK\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n");
+			}
+		}
 	},
 
 	// 执行任务
-	doWrk: function (dat) {
-		// ... 主内容 ...
+	doWrk: function (id, dat) {
+		console.log(id + " : >> " + dat.length);
+		var s = nato.rs[id];
+		if (s) {
+			s.write(dat);
+		}
+	},
 
-		// 连接继续
-		nato.hdRtn();
+	// 清除子连接
+	clrSub: function (id) {
+		if (nato.rs[id]) {
+			console.log("End : " + id);
+			nato.rs[id].end();
+			delete nato.rs[id];
+		}
+	},
+
+	// 获取ID
+	getId: function (s) {
+		var id = 0, k = 0;
+		do {
+			id = (++ nato.sn);
+			if (id > nato.max) {
+				if (k) {
+					id = 0;
+				} else {
+					k = 1;
+					nato.sn = 0;
+					id = (++ nato.sn);
+				}
+			}
+		} while (id && nato.rs[id]);
+		if (id) {
+			s.id = id;
+			nato.rs[id] = s;
+		}
+		return id;
+	},
+
+	// 关闭子连接
+	endDat: function () {
+		var id = this.id;
+		this.removeAllListeners("data");
+		this.removeAllListeners("error");
+		this.removeAllListeners("end");
+		this.on("error", nato.hdErr);
+		this.id = 0;
+		this.end();
+		if (id) {
+			nato.sendWrk(nato.clsBuf.from(id + ".0."));
+			nato.clrSub(id);
+		}
 	},
 
 	// 添加任务
-	addWrk: function (s, dat) {
-		// ... 主内容 ...
-
-		nato.send(s, "oOK", nato.clsBuf.from("OK!"));
-		s.end();
-
-		// 若处于心跳期，则停止心跳，发送任务
-		if (nato.keepLink) {
-			clearTimeout(nato.keepLink);
-			nato.keepLink = 0;
-			nato.sendWrk();
-		}
+	hdDat: function (dat) {
+		console.log(this.id + " : << " + dat.length);
+		nato.sendWrk(nato.clsBuf.concat([
+			nato.clsBuf.from(this.id + "." + dat.length + "."),
+			dat
+		]));
 	},
 
-	// 发送信息
-	send: function (s, nam, dat) {
-		if (s) {
-			var d = "HTTP/1.1 200 " + nam + "\r\nConnection: keep-alive\r\nContent-Length: ";
-			if (dat) {
-				d = nato.clsBuf.concat ([
-					nato.clsBuf.from (d + dat.length + "\r\n\r\n"),
-					dat
-				]);
-			} else {
-				d += "0\r\n\r\n";
+	// 发送任务
+	sendWrk: function (dat) {
+		if (nato.socket) {
+			if (nato.keepLink) {
+				clearTimeout(nato.keepLink);
+				nato.keepLink = 0;
 			}
-			s.write(d);
+			nato.socket.write(dat);
+			nato.kepLnk();
 		}
-	},
-
-	// 测试回复
-	test: function (s, msg) {
-		s.write("HTTP/1.1 503\r\nConnection: close\r\nContent-Length: " + msg.length + "\r\n\r\n" + msg);
-		s.end();
 	},
 
 	// 错误处理
 	hdErr: function (e) {
+		this.removeAllListeners("data");
+		this.removeAllListeners("error");
+		this.removeAllListeners("end");
 		this.end();
 	}
 };
 
 
-net.createServer(nato.listener).listen(nato.port);
+var srv = net.createServer(nato.listener).listen(nato.port);
+// srv.maxConnections = 1;
 console.log("LZRnatsrv start in " + nato.port);
